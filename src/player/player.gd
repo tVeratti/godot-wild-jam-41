5013635c29b8828f412a22d7cb58062094f8837b
Tatiana Veratti <TatianaVeratti@gmail.com>
2022-01-22 19:08:36 -0800

    add pathing to world coords

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,9 +1,36 @@
-extends Node
+extends KinematicBody2D
+
 
 signal navigate(origin, destination)
+signal movement_complete()
+
+const MOVE_SPEED = 100
+
+
+var path:Array = []
+
+var velocity:Vector2 = Vector2.ZERO
+var target_position:Vector2 = Vector2.ZERO
+
+
+func _physics_process(delta):
+	if target_position.distance_to(position) > 5:
+		velocity = position.direction_to(target_position).normalized() * MOVE_SPEED
+		move_and_slide(velocity)
+	else:
+		emit_signal("movement_complete")
+		set_physics_process(false)
 
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
-		var player_position = Vector2(150, 150) # TEMP
-		emit_signal("navigate", player_position, event.position)
+		emit_signal("navigate", position, event.position)
+
+
+func move_along_path():
+	if path.empty():
+		set_physics_process(false)
+	else:
+		set_physics_process(true)
+		target_position = path.pop_front()
+	
